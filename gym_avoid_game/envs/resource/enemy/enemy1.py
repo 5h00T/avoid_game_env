adfd4ef7279f6a1b99cf7baf7712998825697ec5
SHOOT <undefindshinjin@gmail.com>
2019-02-26 19:46:05 +0900

    remove debug print

--- gym_avoid_game/envs/resource/enemy/enemy1.py
+++ gym_avoid_game/envs/resource/enemy/enemy1.py
@@ -6,7 +6,6 @@ class Enemy1(enemy.Enemy):
         super().__init__(x, y, width, height, hp, color)
         self.shot_positions.append(enemy.ShotPosition(self.x, self.y))
         self.shot_functions.append(self.shot_positions[0].pattern4(15, 24, 0.5, 20))
-        print("DD")
 
     def update(self):
         super().update()
