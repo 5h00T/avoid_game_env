ce6ee29f487447c420333507f5e98cffae2581e3
SHOOT <undefindshinjin@gmail.com>
2019-02-26 14:29:56 +0900

    fix

--- gym_avoid_game/envs/avoid_game_env.py
+++ gym_avoid_game/envs/avoid_game_env.py
@@ -46,6 +46,7 @@ class ShootingEnv(gym.Env):
             self.screen = pygame.display.set_mode((60, 60))
         except:
             os.environ["SDL_VIDEODRIVER"] = "dummy"
+            self.screen = pygame.display.set_mode((60, 60))
         pygame.display.set_caption("shooting_env")
 
         self.screen.fill((255, 255, 255))
