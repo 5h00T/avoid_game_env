5015e9aa5368f0cace8f84c3b8162357d9acc5bb
SHOOT <undefindshinjin@gmail.com>
2019-02-27 03:05:01 +0900

    rename packages

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name="gym_avoid_game",
       version="0.0.1",
-      packages=find_packages("avoid_game_env"),
+      packages=find_packages("gym_avoid_game"),
       install_requires=["gym", "pygame"]
 )
