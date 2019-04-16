d41e39d131124f7a1f82de7a30a1ab1311331cb2
SHOOT <undefindshinjin@gmail.com>
2019-04-16 14:43:45 +0900

    reformat files

--- gym_avoid_game/envs/resource/enemy/enemy2.py
+++ gym_avoid_game/envs/resource/enemy/enemy2.py
@@ -11,7 +11,9 @@ class Enemy2(enemy.Enemy):
         first_angle = random.randint(0, 30)
         for start_angle in range(first_angle, first_angle + 360, 30):
             self.set_shot_function(
-                self.shot_positions[0].pattern19(start_angle, 3.3, 10, 0, math.inf, 1, 0, 40, lambda count: -math.sin(self.count / 120) * 20, lambda count: math.sin(self.count / 120) * 3.5)
+                self.shot_positions[0].pattern19(start_angle, 3.3, 10, 0, math.inf, 1, 0, 40,
+                                                 lambda count: -math.sin(self.count / 120) * 20,
+                                                 lambda count: math.sin(self.count / 120) * 3.5)
             )
 
     def update(self):
